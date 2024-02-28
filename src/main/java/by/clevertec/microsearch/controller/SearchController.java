84a98922d07e60761812bdc738b7f1095befd16c
Viktar Rusakovich <mrisviz.rus@gmail.com>
2024-02-28 19:04:52 +0300

    feat: fix mapping

--- src/main/java/by/clevertec/microsearch/controller/SearchController.java
+++ src/main/java/by/clevertec/microsearch/controller/SearchController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/search")
+@RequestMapping("/search")
 @RequiredArgsConstructor
 public class SearchController {
 
